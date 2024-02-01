99a53156e5e73a719e37fcabda4353682b7c00ed
Giwa Muhammad <mgiwa78@gmail.com>
2024-02-01 14:41:41 +0100

    Baseline

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { ConfigureStoreOptions, configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import propertyReducer from "./slice/propertySlice";
+import { api } from "@/services/api";
+import logger from "redux-logger";
 
-export const makeStore = () => {
+export const makeStore = (
+  options?: ConfigureStoreOptions["preloadedState"] | undefined
+) => {
   return configureStore({
     reducer: {
-      property: propertyReducer,
+      [api.reducerPath]: api.reducer,
       auth: authReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware, logger),
+    ...options
   });
 };
 
